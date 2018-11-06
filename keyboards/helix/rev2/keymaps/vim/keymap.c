a7494bce24b160de3c2d395c6a506b63954090bd
g-hyoga <hyoga0216@gmail.com>
2018-11-06 19:58:01 +0900

    ...

--- keyboards/helix/rev2/keymaps/vim/keymap.c
+++ keyboards/helix/rev2/keymaps/vim/keymap.c
@@ -28,6 +28,7 @@ enum layer_number {
     _BASE = 0,
     _NORMAL,
     _INSERT,
+    _LOWER,
     _RAISE,
     _ADJUST
 };
@@ -36,11 +37,10 @@ enum custom_keycodes {
   BASE = SAFE_RANGE,
   NORMAL,
   INSERT,
+  LOWER,
   RAISE,
   ADJUST,
   BACKLIT,
-  EISU,
-  KANA,
   RGBRST
 };
 
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------.             ,-----------------------------------------.
    * | Tab  |      |      |      |      |      |             |      |      |Insert|      |      | Bksp |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Ctrl |      |      |      |      |      |             | Left | Down |  Up  |Right |      |      |
+   * | Ctrl |Insert|      |      |      |      |             | Left | Down |  Up  |Right |      |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Shift|      |      |      |      |      |             |      |      |      |      |      |      |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_NORMAL] = LAYOUT( \
       KC_TAB,  _______, _______, _______, _______, _______,                    _______, _______, INSERT,  _______,  _______, KC_BSPC,
-      KC_LCTL, _______, _______, _______, _______, _______,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, \
+      KC_LCTL, INSERT,  _______, _______, _______, _______,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, \
       KC_LSFT, _______, _______, _______, _______, _______,                    _______, _______, _______, _______,  _______, _______, \
       _______, _______, KC_LALT, KC_LGUI, _______, KC_SPACE, _______, _______, KC_ENT,  RAISE,   KC_RGUI, KC_RALT,  _______, _______ \
       ),
@@ -89,32 +89,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |  -   |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |Normal|      |      |      |      |Space |      |      |Enter |Raise |      |      |      |      |
+   * |Normal| xxxx | xxxx | xxxx |Lower |Space |Normal|      |Enter |Raise | xxxx | xxxx | xxxx | xxxx |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_INSERT] = LAYOUT( \
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC, \
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                       KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_MINS , \
-      NORMAL,  _______, _______, _______, _______, KC_SPACE, _______, _______, KC_ENT,  RAISE,  _______, _______, _______, _______ \
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC, \
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                     KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_MINS , \
+      NORMAL,  _______, INSERT,  INSERT,  LOWER, KC_SPACE, NORMAL, _______,  KC_ENT,  RAISE, INSERT,  INSERT,  _______, _______ \
+      ),
+
+  /* Lower
+   * ,-----------------------------------------.             ,-----------------------------------------.
+   * |      |      |   1  |   2  |   3  |      |             |      |   _  |   =  |   (  |   )  | Bksp |
+   * |------+------+------+------+------+------|             |------+------+------+------+------+------|
+   * |      |      |   4  |   5  |   6  |      |             | Left | Down |  Up  |Right |      |  |   |
+   * |------+------+------+------+------+------|             |------+------+------+------+------+------|
+   * |      |      |   7  |   8  |   9  |   0  |             |      |      |      |      |      |      |
+   * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
+   * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
+   * `-------------------------------------------------------------------------------------------------'
+   */
+  [_LOWER] = LAYOUT( \
+      _______, _______, KC_1,    KC_2,    KC_3,    _______,                   _______, KC_UNDS, KC_EQL, KC_LPRN,  KC_RPRN, KC_BSPC, \
+      _______, _______, KC_4,    KC_5,    KC_6,    _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, KC_PIPE, \
+      _______, _______, KC_7,    KC_8,    KC_9,    KC_0,                      _______, _______, _______, _______,  _______, _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______ \
       ),
 
   /* Raise
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * |   `  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  | Del  |
+   * | Ecs  |      |      |      |      |      |             |   ~  |   !  |   @  |   #  |   $  | Bksp |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |   -  |   =  |   [  |   ]  |  \   |
+   * |      |      | COPY | CUT  |PASTE |      |             |   %  |   ^  |   &  |   *  |      |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |  F7  |  F8  |  F9  |  F10 |  F11 |             |  F12 |      |      |PageDn|PageUp|      |
+   * |      |      |      |      |      |      |             |   `  |   \  |      |      |      |      |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
+   * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_RAISE] = LAYOUT( \
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,                    KC_F12,  _______, _______, KC_PGDN, KC_PGUP, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+      KC_ESC,  _______, _______, _______, _______, _______,                   KC_TILD, KC_EXLM, KC_AT,    KC_HASH, KC_DLR,  KC_BSPC, \
+      _______, _______, M(0),    M(1),    M(2),    _______,                   KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR, _______, _______, \
+      _______, _______, _______, _______, _______, _______,                   KC_GRV,  KC_BSLS, _______,  _______, _______, _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______ \
       ),
 
   /* Adjust (Lower + Raise)
@@ -177,26 +195,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_off(_INSERT);
+        layer_on(_LOWER);
+      } else {
+        layer_off(_LOWER);
+        layer_on(_INSERT);
+      }
+      return false;
+      break;
     case RAISE:
       if (record->event.pressed) {
-        //not sure how to have keyboard check mode and set it to a variable, so my work around
-        //uses another variable that would be set to true after the first time a reactive key is pressed.
-        if (TOG_STATUS) { //TOG_STATUS checks is another reactive key currently pressed, only changes RGB mode if returns false
-        } else {
-          TOG_STATUS = !TOG_STATUS;
-          #ifdef RGBLIGHT_ENABLE
-            //rgblight_mode(RGBLIGHT_MODE_SNAKE);
-          #endif
-        }
+        layer_off(_INSERT);
         layer_on(_RAISE);
-        update_tri_layer_RGB(_INSERT, _RAISE, _ADJUST);
       } else {
-        #ifdef RGBLIGHT_ENABLE
-          //rgblight_mode(RGB_current_mode);  // revert RGB to initial mode prior to RGB mode change
-        #endif
         layer_off(_RAISE);
-        TOG_STATUS = false;
-        update_tri_layer_RGB(_INSERT, _RAISE, _ADJUST);
+        layer_on(_INSERT);
       }
       return false;
       break;
@@ -298,6 +313,7 @@ void matrix_update(struct CharacterMatrix *dest,
 #define L_BASE 0
 #define L_NORMAL (1<<_NORMAL)
 #define L_INSERT (1<<_INSERT)
+#define L_LOWER (1<<_LOWER)
 #define L_RAISE (1<<_RAISE)
 #define L_ADJUST (1<<_ADJUST)
 #define L_ADJUST_TRI (L_ADJUST|L_RAISE|L_INSERT)
@@ -342,6 +358,9 @@ void render_status(struct CharacterMatrix *matrix) {
            break;
         case L_INSERT:
            matrix_write_P(matrix, PSTR("Insert"));
+           break;
+        case L_LOWER:
+           matrix_write_P(matrix, PSTR("Lower"));
            break;
         case L_RAISE:
            matrix_write_P(matrix, PSTR("Raise"));
